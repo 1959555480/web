d80db51b0806bb0a578fb945b37cb435400cef90
yyx <1959555480@qq.om>
2019-05-09 15:38:21 +0800

    vue路由带注释

---
+++ vue-demo/view/first.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="first">
+        first内容
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
+
---
+++ vue-demo/view/good.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        货物{{ $route.query.goodId }}
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
