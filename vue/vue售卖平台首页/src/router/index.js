388fe504c140c22ae3f0a503c1e3e48e8d8589b9
yyx <1959555480@qq.com>
2019-07-02 22:48:13 +0800

    子路由子页面添加

--- vue/vue售卖平台首页/src/router/index.js
+++ vue/vue售卖平台首页/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../components/pages/index'
+import DetailsIndex from '../components/pages/details/detailsIndex'
+import sellPage from '../components/pages/details/sell'
+import indexPage from '../components/pages/details/sell'
+import productPage from '../components/pages/details/product'
 
 Vue.use(Router)
 
@@ -10,6 +14,20 @@ export default new Router({
       // 试图渲染到上一级的router-view
       path: '/',
       component: IndexPage
-    }
+    },
+    {
+      path: '/details',
+      component: DetailsIndex,
+      children:[
+        {
+          path: 'sell',
+          component: sellPage
+        },
+        {
+          path: 'product',
+          component: productPage
+        }
+      ]
+    },
   ]
 })
