cb9bab39106c7a95f5c4f0f0ecc1a331f5e93727
yyx <1959555480@qq.com>
2019-09-06 23:53:55 +0800

    第一天

---
+++ js常见基础问题/Day1/index.js
@@ -0,0 +1,18 @@
+// 数组
+var arr = [];
+// 生成2-32内的随机数
+var num = 2 + Math.floor(Math.random()*30);
+// 记录回调次数
+var n = 0;
+var setArr = function(num){
+    if(arr.length === 5{
+        return console.log(arr);
+    }
+    if(arr.every(v=>{v!==num}){
+     arr.push(num);   
+    }//判断数组是否没有该值
+    n++;
+    setArr(Math.random()*Math.random()*31+x);
+}
+
+setArr(num)
