13f75a8012aa74fc4c413bf210b2f2a5ae55d920
yyx <1959555480@qq.com>
2020-03-26 00:08:16 +0800

    leetcode_easy_罗马数字转整数

--- leetcode/easy/13.罗马数字转整数.js
+++ leetcode/easy/13.罗马数字转整数.js
@@ -14,14 +14,15 @@ var romanToInt = function (s) {
   var s = s.split('')
   let result = 0
   for(let i = 0; i<s.length; i++) {
-    let j = i 
-    if(s[i] == 'I'&&s[i+1] =='V'){result+=4;i++}
-    if(s[i] == 'I'&&s[i+1] =='X'){result+=9;i++}
-    if(s[i] == 'X'&&s[i+1] =='L'){result+=40;i++}
-    if(s[i] == 'X'&&s[i+1] =='C'){result+=90;i++}
-    if(s[i] == 'C'&&s[i+1] =='D'){result+=400;i++}
-    if(s[i] == 'C'&&s[i+1] =='M'){result+=900;i++}
-    if(i == j) {
+    let flag = true 
+    if(s[i] == 'I'&&s[i+1] =='V'){result+=4;i++; flag = false}
+    if(s[i] == 'I'&&s[i+1] =='X'){result+=9;i++; flag = false}
+    if(s[i] == 'X'&&s[i+1] =='L'){result+=40;i++; flag = false}
+    if(s[i] == 'X'&&s[i+1] =='C'){result+=90;i++; flag = false}
+    if(s[i] == 'C'&&s[i+1] =='D'){result+=400;i++; flag = false}
+    if(s[i] == 'C'&&s[i+1] =='M'){result+=900;i++; flag = false}
+    // 上下两边只能进入一次
+    if(flag) {
       if(s[i] == "I") { result += 1}
       if(s[i] == "V") { result += 5}
       if(s[i] == "X") { result += 10}
@@ -34,6 +35,6 @@ var romanToInt = function (s) {
   return result
 };
 
-console.log( romanToInt('IV') )
+console.log( romanToInt('MCMXCIV') )
 // @lc code=end
 
