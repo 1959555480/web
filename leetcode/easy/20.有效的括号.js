b7607d1556d7ab5ed6fb8f8854f2d34d2878cc01
yyx <1959555480@qq.com>
2020-03-28 00:14:39 +0800

    leetcode_easy_有效的括号

--- leetcode/easy/20.有效的括号.js
+++ leetcode/easy/20.有效的括号.js
@@ -18,27 +18,28 @@ var isValid = function (s) {
   if(s.length%2!=0){
     return false
   }
+  if(s.length==0){
+    return true
+  }
+  var arr = []
   for (let i = 0; i < s.length; i++){
-    let arr = []
     let nowString = s.charAt(i)
     if(nowString in bracketsJson){
-      // 属于结束的右边符号  拿arr最后一个符号对比 是否属于一对
+      // // 属于结束的右边符号  拿arr最后一个符号对比 是否属于一对
       let el = arr.pop()//存起来的左符号的最后一个
-      console.log("123",arr,nowString,el,bracketsJson[nowString])
       if(el!=bracketsJson[nowString]){
-        console.log("不成立")
+        // console.log("不成立")
         return false
-      }else{
-        console.log("成立")
-        return true
       }
     }else{
       // 属于开始的左边符号  存起来到arr里
       arr.push(nowString)
+      console.log("arr",arr)
     }
   }
+  return arr.length == 0;
 };
-console.log(isValid("{[]}"))
+console.log(isValid("(()("))
 // @lc code=end
 
 // c      stack        template
