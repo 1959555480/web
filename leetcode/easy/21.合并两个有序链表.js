08a4f30db2f280d56b4539cc592b8c7c1a77cb98
yyx <1959555480@qq.com>
2020-03-29 01:40:22 +0800

    leetcode_easy_合并两个有序链表

--- leetcode/easy/21.合并两个有序链表.js
+++ leetcode/easy/21.合并两个有序链表.js
@@ -19,25 +19,46 @@
  */
 var mergeTwoLists = function (l1, l2) {
   var mergedHead = {
-      val: -1,
-      next: null
-    },
-    crt = mergedHead;
+    name: 'head',
+    val: -1,
+    next: null
+  }
+  var cur = mergedHead
   while (l1 && l2) {
     if (l1.val > l2.val) {
-      crt.next = l2;
-      l2 = l2.next;
+      cur.next = l2
+      l2 = l2.next
     } else {
-      crt.next = l1;
-      l1 = l1.next;
+      cur.next = l1
+      l1 = l1.next
     }
-    crt = crt.next;
+    cur = cur.next
   }
-  crt.next = l1 || l2;
-
+  cur.next = l1 || l2
   return mergedHead.next;
 };
-mergeTwoLists([1,2,4],
-  [1,3,4])
+console.log(
+  mergeTwoLists(
+    {
+      val: 1,
+      next: {
+        val: 2,
+        next: {
+          val: 4,
+          next: null
+        }
+      }
+    },
+    {
+      val: 1,
+      next: {
+        val: 3,
+        next: {
+          val: 4,
+          next: null
+        }
+      }
+    },
+  )
+)
 // @lc code=end
-
