1493675eb79cc6f3492575afecbc1347b94acb10
yyx <1959555480@qq.com>
2020-04-07 00:18:41 +0800

    测试

--- leetcode/easy/66.加一.js
+++ leetcode/easy/66.加一.js
@@ -14,5 +14,6 @@ var plusOne = function(digits) {
   let newArr = c.map(Number)
   return newArr
 };
+// ES10中，规范新引入了一种基本数据类型BigInt。它可以完美地兼容大数。
 // @lc code=end
-console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+// console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
