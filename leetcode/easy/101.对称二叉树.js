0010cc73b168253dab157438ea547472d2ce5c2d
yyx <1959555480@qq.com>
2020-04-12 18:47:24 +0800

    leetcode_easy_对称二叉树

--- leetcode/easy/101.对称二叉树.js
+++ leetcode/easy/101.对称二叉树.js
@@ -16,8 +16,41 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {
-    
+// let root = {
+//   val: 1,
+//   left: {
+//     val: 2,
+//     left: { val: 3, left: null, right: null, },
+//     right: { val: 4, left: null, right: null, },
+//   },
+//   right: {
+//     val: 2,
+//     left: { val: 4, left: null, right: null, },
+//     right: { val: 3, left: null, right: null, }
+//   },
+// }
+var isSymmetric = function (root) {
+  if (root==null) {
+    return true
+  }else{
+    return check(root.left, root.right)
+  }
 };
+// 用于比较两颗二叉树是否相等
+function check(left, right) {
+  if (left == null && right == null) {
+    return true//两个都是空 对此
+  }
+  if (left === null || right === null) {//一空一非空 肯定不对称
+    return false
+  }
+  if (left.val == right.val) {//最后看值是否相等
+    //相等 继续递归
+    return check(left.left, right.right) && check(left.right, right.left)
+  }else{
+    return false
+  }
+}
+// console.log(isSymmetric([]))
 // @lc code=end
 
