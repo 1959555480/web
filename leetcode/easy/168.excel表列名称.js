66c288292db18925c9ec4f7a6e0a3c0a4ecd4505
yyx <1959555480@qq.com>
2020-04-20 21:40:13 +0800

    leetcode_easy_168Excel表列长度

---
+++ leetcode/easy/168.excel表列名称.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=javascript
+ *
+ * [168] Excel表列名称
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+  // let num = String.fromCharCode(n+64)
+  let str = ''
+  if(n==0){
+    return ''
+  }else{
+    while(n!==0){
+      n = n-1//26进制
+      temp = n%26//余数
+      n = Math.floor(n/26)//整数
+      str = String.fromCharCode(temp+65) +str
+    }
+    return str
+    // console.log(str)
+  }
+  
+};
+// convertToTitle(701)
+// @lc code=end
+
