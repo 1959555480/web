5760560c3b906ba98e65ca41aa5d0edb921af4f6
yyx <1959555480@qq.com>
2020-04-30 23:28:02 +0800

    leetcode_easy_202.快乐数

--- leetcode/easy/202.快乐数.js
+++ leetcode/easy/202.快乐数.js
@@ -9,8 +9,20 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n) {
-    1
+var isHappy = function (n) {
+	let set = new Set()
+	n = n + ''
+	let sum = 0
+	while (sum !== 1) {
+		sum = 0
+		for (let i = 0; i < n.length; i++) {
+			sum = sum + n[i] * n[i]
+		}
+		if (set.has(sum)) return false
+		n = sum + ''
+		set.add(sum)
+	}
+	return true
 };
 // @lc code=end
 
