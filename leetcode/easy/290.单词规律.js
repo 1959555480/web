247f4d19d242669be714713c3905ca0e3e94ed39
yyx <1959555480@qq.com>
2020-05-16 23:27:43 +0800

    leetcode_easy_290.单词规律

---
+++ leetcode/easy/290.单词规律.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=290 lang=javascript
+ *
+ * [290] 单词规律
+ */
+
+// @lc code=start
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    1
+};
+// @lc code=end
+
