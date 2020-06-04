9e5c0f9db9d078f14b74a56aa1c8bc36b5deeaba
yyx <1959555480@qq.com>
2020-06-04 22:10:48 +0800

    leetcode_easy_412.fizz-buzz

--- leetcode/easy/412.fizz-buzz.js
+++ leetcode/easy/412.fizz-buzz.js
@@ -11,19 +11,25 @@
  */
 var fizzBuzz = function (n) {
   let arr = []
-  for (let i = 1; i < n + 1;i++){
-    if((i%3==0)&&(i%5==0)){
-      arr.push('FizzBuzz')
-    }else if(i%3==0){
-      arr.push('Fizz')
-    }else if(i%5==0){
-      arr.push('Buzz')
-    }else{
-      arr.push(String(i))
-    }
+  for (let i = 1; i < n + 1; i++) {
+    // if((i%3==0)&&(i%5==0)){
+    //   arr.push('FizzBuzz')
+    // }else if(i%3==0){
+    //   arr.push('Fizz')
+    // }else if(i%5==0){
+    //   arr.push('Buzz')
+    // }else{
+    //   arr.push(String(i))
+    // }
+    // 简化
+    let str = ''
+    str += i % 3 == 0 ? 'Fizz' : ''
+    str += i % 5 == 0 ? 'Buzz' : ''
+    if (!str) str += i 
+    arr.push(str)
   }
   return arr
 };
-// console.log(fizzBuzz(15))
+console.log(fizzBuzz(15))
 // @lc code=end
 
