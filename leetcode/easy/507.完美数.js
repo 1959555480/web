6cf0101ede9a0eabef3bd309a8b1fca991796954
yyx <1959555480@qq.com>
2020-06-24 23:02:21 +0800

    leetcode_easy_507.完美数

---
+++ leetcode/easy/507.完美数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=507 lang=javascript
+ *
+ * [507] 完美数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+  // if (num < 1) return false;
+  // var sum = 0;
+  // for (var i = 1; i < num; i++) {
+  //   if (num % i === 0) {
+  //     sum += i;
+  //   }
+  // }
+  // return num === sum;
+  
+  // 法二
+  // 完美数就几个
+  // return num === 6 || num === 28 || num === 496 || num === 8128 || num === 33550336;
+};
+// @lc code=end
+
