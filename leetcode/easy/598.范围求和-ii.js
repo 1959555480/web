86d4d117659b45700e8d590bb164f9548465fc34
yyx <1959555480@qq.com>
2020-07-07 22:29:17 +0800

    leetcode_easy_598.范围求和-ii

--- leetcode/easy/598.范围求和-ii.js
+++ leetcode/easy/598.范围求和-ii.js
@@ -11,8 +11,22 @@
  * @param {number[][]} ops
  * @return {number}
  */
-var maxCount = function(m, n, ops) {
-231
+var maxCount = function (m, n, ops) {
+  let xarr = [];
+  let yarr = [];
+  if (ops.length === 0) return m * n;
+  for (let i = 0; i < ops.length; i++) {
+    const val = ops[i];
+    xarr.push(val[0]);
+    yarr.push(val[1]);
+  }
+  xarr.sort((a, b) => a - b);
+  yarr.sort((a, b) => a - b);
+
+  return xarr[0] * yarr[0]
+  // console.log(xarr[0] * yarr[0])
 };
+
+// maxCount(3, 3,[[2,2],[3,3]])
 // @lc code=end
 
