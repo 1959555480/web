d5b724889dcabfe0aa74b638832f320218a0b8fd
yanyongxing <1959555480@qq.com>
2021-09-27 09:59:52 +0800

    997.找到小镇的法官

--- leetcode/easy/997.找到小镇的法官.js
+++ leetcode/easy/997.找到小镇的法官.js
@@ -28,6 +28,8 @@ var findJudge = function (n, trust) {
   trust.forEach((i, index) => {
     unableArr.push(i[0])
   })
+  console.log("不可能：",unableArr)
+  console.log("可能：",maybeArr)
   // 统计最多的
   let target = new Map()
   for (let i = 0; i < maybeArr.length; i++) {
@@ -38,24 +40,24 @@ var findJudge = function (n, trust) {
     }
   }
   let max = Math.max(...Array.from(target.values()))
+  console.log("出现最多的",max)
   // 判断
   if (max = n - 1) {
     // 符合
     for (let [key, value] of target.entries()) {
       if (value === max){
+        // 根据value 获取 key
         if(unableArr.indexOf(key)!==-1){
           return -1
         }
         return key
-      }else{
-        return -1
-      }
-        
+      }  
     }
   } else {
     return -1
   }
+  return -1
 };
-console.log(findJudge(3, [[1,3],[2,3],[3,1]]))
+console.log(findJudge(4, [[1,2],[1,3],[2,1],[2,3],[1,4],[4,3],[4,1]]))
 // @lc code=end
 
